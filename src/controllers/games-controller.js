3214fbf09ef4b154609634c700aebfeda7dc7b6c
MartinDeep13 <martin0013esp@hotmail.com>
2020-03-25 12:09:21 -0600

    txt

--- src/controllers/games-controller.js
+++ src/controllers/games-controller.js
@@ -22,7 +22,7 @@ const createGame = async (req, res) => {
 
 const deleteGame = async  (req, res) => {
     const games = await Games.find();
-        res.send({status:'OKis',data:games})
+        res.send({status:'OKasdasdasdasdis',data:games})
 };
 
 const getGames = async (req, res) => {
