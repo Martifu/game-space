4fdf4138e8c62c6a9de81ef84453ce04e6244256
DaniGadget18 <dani_181298@hotmail.com>
2020-04-07 22:19:48 -0500

    correciones

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/login', usersController.login);
 router.post('/create', usersController.createUser);
-router.post('/update', isValidHostname, isAuth, usersController.updateUser);
+router.post('/update', isAuth, usersController.updateUser);
 router.post('/delete', usersController.deleteUser);
 router.get('/get-users', usersController.getUsers);
 router.get('/getusersById/:id', usersController.userById);
