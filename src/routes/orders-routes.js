b6b8d15eb3fbb61719c3585b57bb00e9abfca8c0
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2020-04-14 17:33:08 -0500

    Update orders-routes.js

--- src/routes/orders-routes.js
+++ src/routes/orders-routes.js
@@ -4,5 +4,5 @@ const ordersController = require('../controllers/orders-controller');
 const router = express.Router();
 
 router.post('/create', ordersController.create);
-router.post('/getTotal', ordersController.getTot);
+router.get('/getTotal', ordersController.getTot);
 module.exports = router;
