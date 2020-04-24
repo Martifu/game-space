4f25638069083c9ba13e25ef56e71a28d21459ac
DaniGadget18 <dani_181298@hotmail.com>
2020-04-24 03:25:16 -0500

    emails

--- src/configemail/configMensaje.js
+++ src/configemail/configMensaje.js
@@ -11,9 +11,9 @@ module.exports = (data) => {
  });
 
 const mailOptions = {
- from: `”${data.customer_name} 👻” <${data.mail}>`,
+ from: `Compra con exito - Game Spaces 2020`,
  to: `${data.mail}`, // Cambia esta parte por el destinatario
- subject:'Compra con exito - Game Spaces 2020',
+ subject:'Game Spaces 2020',
  html: `
  <strong>Nombre:</strong> ${data.customer_name} <br/>
  <strong>E-mail:</strong> ${data.mail} <br/>
@@ -25,38 +25,24 @@ const mailOptions = {
  <h2>La compra se hizo con exito.</h2>
 
  <table style="max-width: 600px; padding: 10px; margin:0 auto; border-collapse: collapse;">
- <tr>
-    <td style="background-color: #ecf0f1; text-align: left; padding: 0">
-    <div> Tu orden # ${data.id} </div>
-    </td>
- </tr>
-
- <tr>
-    <td style="padding: 0">
-       <img style="padding: 0; display: block" src="../assets/img/ipgone.png" width="50%">
-    </td>
- </tr>
  
  <tr>
-    <td style="background-color: #ecf0f1">
+    <td style="background-color: #642EFE">
        <div style="color: #34495e; margin: 4% 10% 2%; text-align: justify;font-family: sans-serif">
-          <h2 style="color: #e67e22; margin: 0 0 7px">Hola Poketrainer!</h2>
+          <h2 style="color: #00FF80; margin: 0 0 7px">Tu orden # ${data.id} </h2>
           <p style="margin: 2px; font-size: 15px">
              Gracias por la compra en nuestra tienda de juegos
-          <ul style="font-size: 15px;  margin: 10px 0">
-             <li>Batallas amistosas.</li>
-             <li>Torneos Oficiales.</li>
-             <li>Intercambios de Pokémon.</li>
-             <li>Actividades de integración.</li>
-             <li>Muchas sorpresas más.</li>
-          </ul>
+          <div>
+          Puedes descargar la app y obtener mas juegos
+          :)
+          </div>
           <div style="width: 100%;margin:20px 0; display: inline-block;text-align: center">
             
           </div>
           <div style="width: 100%; text-align: center">
             
           </div>
-          <p style="color: #b3b3b3; font-size: 12px; text-align: center;margin: 30px 0 0">Poketrainers Trujillo 2016</p>
+          <p style="color: #b3b3b3; font-size: 12px; text-align: center;margin: 30px 0 0">Game-Space 2020</p>
        </div>
     </td>
  </tr>
