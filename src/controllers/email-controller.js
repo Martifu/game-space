9c996ba20fa44cf99f55e229632b66cd1b264c68
DaniGadget18 <dani_181298@hotmail.com>
2020-04-24 03:56:34 -0500

    emails

--- src/controllers/email-controller.js
+++ src/controllers/email-controller.js
@@ -3,15 +3,8 @@ const confmensaje = require('../configemail/configMensaje');
 const sendMensaje = async (req, res) => {
 
     try {
-        let resp = confmensaje(req.body)
-        if (resp) {
+        confmensaje(req.body)
         res.status(200).send({Status:"Ok", message:"Correo enviado"});
-
-        } else {
-        res.status(404).send({Status:"error", message:"Error en el envio del correo"});
-
-        }
-
     } catch (error) {
         res.status(404).send({status:"error", error:error});
     }
